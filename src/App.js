6a11e3e879dd91ecc8c4e5f89df2159c74da6fd6
咱也不知道叫啥\29603 <liyongjiezzz1@gmail.com>
2023-04-21 08:56:44 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from "react-dom/client";
+class Header extends React.Component {
+    render() {
+        return (
+            <h1>菜鸟教程 - 学的不仅是技术，更是梦想！</h1>
+        );
+    }
+}
+export default Header
+
